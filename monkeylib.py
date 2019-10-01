5745dce6adf554e6f46a4c8a3af7e30436a9d4f6
willem <itsme@xs4all.nl>
2019-10-01 22:41:29 +0200

    monkeylib: added launchmonkey, wait_for_monkey. Monkey.send now returns string. keyevent, sendtext, wake, touch, tap now check OK response. tap now uses 'tap' command.

--- monkeylib.py
+++ monkeylib.py
@@ -5,22 +5,25 @@ Provide an interface to the android 'monkey.jar' ui control interface.
 import socket
 import time
 import select
+import re
+
 """
 Commands implemented by the daemon running on the Android device.
 
 wake
+listvar
+getvar <varname>
 type  <string>
 touch {down,up,move} <x> <y>
 tap <x> <y>
 press <keycode>          == key down + key up
+key  {up,down} <keycode>
+
 quit
 deferreturn [event] [timeout (ms)] [command]
 flip {open,close}
 trackball <dx> <dy>
-key  {up,down} <keycode>
 sleep  <msec>
-listvar
-getvar <varname>
 
 listviews
 queryview "viewid" <controlid> <command:gettext,getparent,...>
@@ -42,7 +45,9 @@ class Monkey:
 
     def send(self, cmd, timeout=0.5):
         self.sock.send((cmd + "\n").encode('utf-8'))
-        return self.readuntil(b"\n", timeout)
+        res = self.readuntil(b"\n", timeout)
+        if res:
+            return res.decode('utf-8')
 
     def readuntil(self, char, timeout):
         self.sock.setblocking(0)
@@ -63,13 +68,16 @@ class Monkey:
             return buf
 
     def keyevent(self, key):
-        self.send("press %d" % key)
+        res = self.send("press %d" % key)
+        return res == "OK"
 
     def sendtext(self, txt):
-        self.send("type %s" % txt)
+        res = self.send("type %s" % txt)
+        return res == "OK"
 
     def wake(self):
-        self.send("wake")
+        res = self.send("wake", 1.0)
+        return res == "OK"
 
     def drag(self, frm, to, duration, steps):
         """
@@ -100,23 +108,95 @@ class Monkey:
         self.touch("up", to)
 
     def touch(self, how, pos):
-        self.send("touch %s %d %d" % (how, pos[0], pos[1]))
+        res = self.send("touch %s %d %d" % (how, pos[0], pos[1]))
+        return res == "OK"
 
     def tap(self, pos):
-        self.touch("down", pos)
-        time.sleep(0.05)
-        self.touch("up", pos)
+        res = self.send("tap %d %d" % (pos[0], pos[1]))
+        return res == "OK"
 
     def listvar(self):
         response = self.send("listvar")
-        if not response.startswith(b'OK:'):
+        if not response.startswith('OK:'):
             return
-        return response[3:].decode('utf-8').split(" ")
+        return response[3:].split(" ")
 
     def getvar(self, name):
         response = self.send("getvar %s" % name)
-        if not response.startswith(b'OK:'):
+        if not response.startswith('OK:'):
             # .. maybe raise exception with error msg ..
             return
-        return response[3:].decode('utf-8')
+        return response[3:]
+
+    @staticmethod
+    def launchmonkey(adb):
+        """
+        returns a Monkey object
+        """
+        print("fwd->", adb.forward(12345, 12345))
+
+        # or 'toybox killall'
+        killres = adb.shell("killall com.android.commands.monkey")
+        if killres.find("killall: not found") >= 0:
+            pidline = adb.shell("ps | grep commands.monkey")
+            if pidline:
+                m = re.match(r'^\w+\s+(\d+)', pidline)
+                if m:
+                    pid = int(m.group(1))
+                    killres = adb.shell("kill %d" % pid)
+            else:
+                killres = 'process not found'
+        print("kill->", killres)
+        time.sleep(0.1)
+        monkeycmd = adb.makeshell("monkey -v --script-log --port 12345")
+
+        if not Monkey.wait_for_monkey(monkeycmd):
+            print("Failed to start monkey")
+            return
+        print("monkey active")
+
+        for _ in range(4):
+            mon = Monkey(12345)
+            try:
+                if mon.wake():
+                    break
+            except Exception as e:
+                print("trying -> %s" % e)
+                import traceback
+                traceback.print_exc()
+
+            mon = None
+            time.sleep(0.5)
+
+        if not mon:
+            print("could not connect to Monkey")
+            return
+
+        monkeycmd.close()
+
+        return mon
+
+    @staticmethod
+    def wait_for_monkey(monkeycmd):
+        """
+        wait until the monkey tool has launched without an error.
+        """
+        tstart = time.time()
+        tend = tstart + 10.0
+
+        print("=== waiting for monkey")
+        while time.time() < tend:
+            resp = monkeycmd.read()
+            if resp:
+                print("monkeycmd -> ", resp)
+                if resp.find('Error') >= 0:
+                    return
+                if resp.find(":Monkey:")>=0:
+                    print()
+                    return True
+
+            time.sleep(0.5)
+        print()
+
+
 
